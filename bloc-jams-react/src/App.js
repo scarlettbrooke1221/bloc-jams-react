012582d4617caa18b19036bc06bc5838aee550a0
erica mitchell <ericamitchell1221@gmail.com>
2018-06-19 18:19:30 -0400

    Assignment 11 Routing: Completed

--- bloc-jams-react/src/App.js
+++ bloc-jams-react/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Albums from './components/Albums';
 
 class App extends Component {
   render() {
@@ -12,12 +13,14 @@ class App extends Component {
 	   <nav>
              <Link to='/'>Landing</Link>
              <Link to='/library'>Library</Link>
+			 <Link to='/album'>Albums</Link>
            </nav>
 	  <h1> Bloc Jams </h1>
 	  </header>
         <main>
 		   <Route exact path="/" component={Landing} />
            <Route path="/library" component={Library} />
+		   <Route path="/album" component={Albums} />
 	   </main>
       </div>
     );
---
+++ bloc-jams-react/src/components/Albums.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Albums = () => (
+  <section className="albums">
+    Album will go here
+  </section>
+);
+
+export default Albums;
