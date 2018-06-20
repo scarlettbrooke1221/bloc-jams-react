91024eb6e76578732eceab7c618d93d3353c645d
erica mitchell <ericamitchell1221@gmail.com>
2018-06-20 06:22:55 -0400

    Checkpoint 12 Landing: Completed

--- bloc-jams-react/src/App.js
+++ bloc-jams-react/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 
+
 class App extends Component {
   render() {
     return (
@@ -12,13 +13,13 @@ class App extends Component {
 	   <nav>
              <Link to='/'>Landing</Link>
              <Link to='/library'>Library</Link>
-           </nav>
+	    </nav>
 	  <h1> Bloc Jams </h1>
 	  </header>
         <main>
 		   <Route exact path="/" component={Landing} />
            <Route path="/library" component={Library} />
-	   </main>
+		</main>
       </div>
     );
   }
