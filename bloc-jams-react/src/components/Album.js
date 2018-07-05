f4fa921833d8532e24cd9ec3511f0d9854ce1789
erica mitchell <ericamitchell1221@gmail.com>
2018-07-05 06:38:38 -0400

    Assignment 15 Audio Playback: COMPLETED

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
- import albumData from './../data/albums';
+import albumData from './../data/albums';
 
 class Album extends Component {
-	  constructor(props) {
+	 constructor(props) {
      super(props);
 	 
 	  const album = albumData.find( album => {
        return album.slug === this.props.match.params.slug
      });
- 
-     this.state = {
+
+    this.state = {
       album: album,
       currentSong: album.songs[0],
-      isPlaying: false
+      isPlaying: false,
+      isHovered: false
      };
 	 
 	 this.audioElement = document.createElement('audio');
@@ -42,46 +43,56 @@ class Album extends Component {
      }
    }
    
- 
-   render() {
-     return (
-       <section className="album">
-          <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/> 
-           <div className="album-details">
-            <h1 id="album-title">{this.state.album.title}</h1>
-             <h2 className="artist">{this.state.album.artist}</h2>
-             <div id="release-info">{this.state.album.releaseInfo}</div>
-           </div>
-         </section>
-		 <table id="song-list">
-           <colgroup>
-             <col id="song-number-column" />
-             <col id="song-title-column" />
-             <col id="song-duration-column" />
-           </colgroup>  
-           <tbody>
-		   
-		    {this.state.album.songs.map( (song, index) => 
-                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-					<div>
-				<tr id="song-index"> {index + 1} </tr>
-				<tr id="song-title">{song.title}</tr>
-				<tr id="song-duration">{song.duration}</tr>
-					</div>
-					</tr>
-					
-										)
-										
-		   }	
-	   
-					};
-					
-	     </tbody>
-         </table>
-       </section>
-     );
-   }
- }
+   
+  render() {
+    return (
+      <section className="album">
+      <section id="album-info">
+      <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/> 
+      <div className="album-details">
+      <h1 id="album-title">{this.state.album.title}</h1>
+      <h2 className="artist">{this.state.album.artist}</h2>
+      <div id="release-info">{this.state.album.releaseInfo}</div>
+      </div>
+      </section>
+		<table id="song-list">
+      <colgroup>
+        <col id="song-number-column" />
+        <col id="song-title-column" />
+        <col id="song-duration-column" />
+      </colgroup> 
+
+    <tbody>
+		  {
+			this.state.album.songs.map( (song,index) => 
+      <tr className="song" key={index} onClick={() => this.handleSongClick(song)} 
+        onMouseEnter={ () => this.setState({ isHovered: index+1 }) }
+        onMouseLeave={ () => this.setState({ isHovered: false }) }>
+
+      <td className="song-actions">
+      <button id="song-action-btns">
+      {(this.state.currentSong.title === song.title) ? 
+        <span className={ this.state.isPlaying ? "ion-pause" : "ion-play" }></span>
+        :
+        (this.state.isHovered === index+1) ?
+        <span className="ion-play"></span>
+        :
+        <span className="song-number">{index+1}</span>
+      }
+    </button>
+  </td>
+  <td className="song-title">{song.title}</td>
+  <td className="song-duration">{song.duration}</td>
+        </tr>
+      )
+    }
+      </tbody>
+		  </table>
+      </section>
+  
+    );
+  }
+}
+
 
 export default Album;
