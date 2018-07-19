67966748087d908b196b70bf5e3b8f98e94d8269
erica mitchell <ericamitchell1221@gmail.com>
2018-07-19 17:26:57 -0400

    correct player bar so song starts automatically

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 		<h1> Bloc Jams </h1>
 		</header>
        <main>
-			<Route exact path="/" component={Landing} />
+			<Route exact ="/" component={Landing} />
 			<Route path="/library" component={Library} />
 			<Route path="/album/:slug" component={Album} />
 	   </main>
